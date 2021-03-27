55625ced01a49d272729b41d751ca6e12f9ae991
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-03-27 20:28:15 -0300

    login on

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import expo from 'expo'
-import React from 'react';
+import React, { useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Facebook from 'expo-facebook'
 
 export default function App() {
+  const [isLoggedin, setLoggedinStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  async function getUserAsync() {
+    const { name } = await requestAsync('me');
+    alert(`Hello ${name} 👋`);
+  }
+
+  async function requestAsync(path, token) {
+    let resolvedToken = token;
+    if (!token) {
+      const auth = await Facebook.getAuthenticationCredentialAsync();
+      if (!auth) {
+        throw new Error(
+          'User is not authenticated. Ensure `logInWithReadPermissionsAsync` has successfully resolved before attempting to use the FBSDK Graph API.'
+        );
+      }
+      resolvedToken = auth.token;
+    }
+    const response = await fetch(
+      `https://graph.facebook.com/${path}?access_token=${encodeURIComponent(resolvedToken)}`
+    );
+    const body = await response.json();
+    return body;
+  }
 
   async function logIn() {
     try {
@@ -23,9 +48,15 @@ export default function App() {
       });
       if (type === 'success') {
         // Get the user's name using Facebook's Graph API
-        const response = await fetch(`https://graph.facebook.com/me?fields=email,name,id,first_name,last_name&access_token=${token}`);
-        alert(`Hi ${response.data}!`);
-        console.log(response)
+        fetch(`https://graph.facebook.com/me?access_token=${token}`)
+          .then(response => {
+            getUserAsync();
+            setLoggedinStatus(true);
+            alert(response.data);
+            setUserData(response.data);
+          })
+          
+          .catch(e => console.log(e))
       } else {
         // type === 'cancel'
       }
@@ -33,17 +64,21 @@ export default function App() {
       alert(`Facebook Login Error: ${message}`);
     }
   }
-
   return (
+    
     <View style={styles.container}>
       <Text>Login Caralho</Text>
       <Button
       onPress={logIn}
       title='LogIn'/>
       <StatusBar style="auto" />
+
     </View>
+    
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
