84b2667e09105fa3c6f4cf3a3ebc7769e6ada6d7
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-03-28 14:29:18 -0300

    login show

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,37 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState} from 'react';
-import { StyleSheet, Text, View, Image , Button } from 'react-native';
+import React from 'react';
+import { Text, Image, Button } from 'react-native';
+import {ViewContainer, ImageComponent, Name, BagIcon, ImageContainer, ImageMask, ButtonLogIn, TextButton, DescriptionText, DescriptionContainer} from './styles';
+import Background from '../../assets/background.png'
+import Bag from '../../assets/card_travel.png'
+import Flight from '../../assets/flight.png'
+
 import * as Facebook from 'expo-facebook'
 import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
-  const [isLoggedin, setLoggedinStatus] = useState(false);
-  const [userData, setUserData] = useState(null);
   const {navigate} = useNavigation();
-
-  // async function getUserAsync() {
-  //   const res = await requestAsync('me');
-  //   setUserData(res)
-  //   alert(`Hello ${res.name} 👋`);
-  // }
-
-  // async function requestAsync(path, token) {
-  //   let resolvedToken = token;
-  //   if (!token) {
-  //     const auth = await Facebook.getAuthenticationCredentialAsync();
-  //     if (!auth) {
-  //       throw new Error(
-  //         'User is not authenticated. Ensure `logInWithReadPermissionsAsync` has successfully resolved before attempting to use the FBSDK Graph API.'
-  //       );
-  //     }
-  //     resolvedToken = auth.token;
-  //   }
-  //   const response = await fetch(
-  //     `https://graph.facebook.com/${path}?access_token=${encodeURIComponent(resolvedToken)}`
-  //   );
-  //   const body = await response.json();
-  //   return body;
-  // }
 
   async function logIn() {
     try {
@@ -53,7 +32,6 @@ export default function Login() {
         fetch(`https://graph.facebook.com/me?access_token=${token}`)
           .then(response => {
             navigate('Home', {token});
-            setLoggedinStatus(true);
           })
           
           .catch(e => console.log(e))
@@ -65,33 +43,42 @@ export default function Login() {
     }
   }
   return (
-    
-    <View style={styles.container}>
-      <Text>Login Caralho</Text>
-      <Button
-      onPress={logIn}
-      title='LogIn'/>
+    <ViewContainer >
+      <ImageContainer>
+        <ImageComponent 
+        source={ Background }
+        />
+        <ImageMask>
+          <Image
+          style={{
+            height: 100,
+          }}
+          source={ Flight }
+          />
+        </ImageMask>
+      </ImageContainer>
+    <DescriptionContainer>
+    <Name>
+        <BagIcon source={ Bag } />
+      <Text style={{fontWeight: 'bold'}}>Smiles</Text>
+      <Text>Travel</Text>
+      </Name>
+
+      <DescriptionText>
+      Acumule milhas através de desafios e 
+      torne o sonho de viajar pelo mundo
+      em realidade.
+      </DescriptionText>
+      <ButtonLogIn 
+      onPress={logIn}>
+        <TextButton>Entrar com o Facebook</TextButton>
+      </ButtonLogIn>
+    </DescriptionContainer>
+      
       <StatusBar style="auto" />
-      {/* {isLoggedin && (
-      <View>
-        <Image style={{ width: 200, height: 200, borderRadius: 50 }}
-      source={{ uri: userData.picture.data.url }}/>
-    <Text style={{ fontSize: 22, marginVertical: 10 }}>Hi {userData.name}!</Text>
-      </View>
-    )} */}
-    </View>
+    </ViewContainer>
     
     
     
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components'
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+export const ViewContainer = styled(View)`
+    display: flex;
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: flex-start;
+`
+export const ImageContainer = styled(View)`
+      position: relative;
+      width: 100%;
+      height: 70%;
+`
+export const ImageMask = styled(View)`
+background: rgba(0, 0, 0, 0.5);
+position: absolute;
+top: 0;
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const ImageComponent = styled(Image)`
+    width: 100%;
+    height: 100%;
+`
+
+
+export const DescriptionContainer = styled(View)`
+width: 100%;
+height:25%;
+display: flex;
+align-items: center;
+justify-content: space-evenly;
+`
+
+export const BagIcon = styled(Image)`
+    width: 24px;
+    height: 24px;
+`
+
+export const Name = styled(View)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const ButtonLogIn = styled(TouchableOpacity)`
+width: 328px;
+height: 56px;
+
+background: #1877F2;
+border-radius: 8px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const TextButton = styled(Text)`
+font-style: normal;
+font-weight: bold;
+font-size: 16px;
+line-height: 20px;
+color: #fff;
+`
+
+export const DescriptionText = styled(Text)`
+width: 60%;
+font-style: normal;
+font-weight: bold;
+font-size: 14px;
+line-height: 17px;
+text-align: center;
+color: #FF7020;
+`
